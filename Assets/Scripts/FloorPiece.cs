c12a29ecb5f940321b5fcdad5b555f42f4b4e23f
Samuel Sarfaty <samuel.sarfaty@gmail.com>
2018-06-02 17:43:03 +0100

    Added textures and material functionality to floor

---
+++ Assets/Scripts/FloorPiece.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorPiece : MonoBehaviour {
+
+	public Material[] mats;
+
+	private Renderer myMat;
+
+	void Awake(){
+		myMat = GetComponent<Renderer> ();
+	}
+
+	void OnMouseOver(){
+		myMat.material = mats [1];
+
+	}
+
+	void OnMouseExit(){
+		myMat.material = mats [0];
+	}
+
+}
