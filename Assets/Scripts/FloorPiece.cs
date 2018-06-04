6327b0ae629c991cde910d25974a68438f41fd03
Samuel Sarfaty <samuel.sarfaty@gmail.com>
2018-06-04 11:28:05 +0100

    Added pipe spawning + rotating on spawn location

--- Assets/Scripts/FloorPiece.cs
+++ Assets/Scripts/FloorPiece.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class FloorPiece : MonoBehaviour {
 
 	public Material[] mats;
+	public GameObject[] pipes;
 
 	private Renderer myMat;
+	private Vector3 pipeSpawnPos;
+	private bool hasPipe;
 
 	void Awake(){
 		myMat = GetComponent<Renderer> ();
+		pipeSpawnPos = new Vector3 (transform.position.x, 2.5f, transform.position.z);
+		hasPipe = false;
 	}
 
 	void OnMouseOver(){
@@ -19,6 +24,15 @@ public class FloorPiece : MonoBehaviour {
 
 	void OnMouseExit(){
 		myMat.material = mats [0];
+	}
+
+	void OnMouseDown(){
+		if (!hasPipe) {
+			Instantiate (pipes [0], pipeSpawnPos, Quaternion.identity);
+			hasPipe = true;
+		} else {
+			print ("pipe already here");
+		}
 	}
 
 }
---
+++ Assets/Scripts/Pipe.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour {
+
+	void OnMouseDown(){
+		transform.Rotate (new Vector3 (transform.rotation.x, transform.rotation.y + 90f, transform.rotation.z));
+	}
+}
