574e51e4910a54037af1bcfac546174c02944600
Samuel Sarfaty <samuel.sarfaty@gmail.com>
2018-06-04 12:45:05 +0100

    UI selects one button at a time

--- Assets/Scripts/PipeButton.cs
+++ Assets/Scripts/PipeButton.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class PipeButton : MonoBehaviour {
 
 	private GameObject selectionFrame;
+	private PipeButton[] buttons;
 
 	void Awake(){
 		selectionFrame = GetComponentInChildren<SelectionFrame> ().gameObject;
+		buttons = GameObject.FindObjectsOfType<PipeButton> ();
 	}
 
 	// Use this for initialization
 	void Start () {
-		selectionFrame.gameObject.SetActive (false);
+		DeSelect ();
 	}
 	
 	public void Select(){
-		selectionFrame.gameObject.SetActive (true);
+		foreach (PipeButton button in buttons) {
+			button.DeSelect ();
+		}
+
+		selectionFrame.SetActive (true);
+	}
+
+	public void DeSelect(){
+		selectionFrame.gameObject.SetActive (false);
 	}
 }
