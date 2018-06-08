88163255bcc5f3dcd8146a7906d35c9ef30c7385
Samuel Sarfaty <samuel.sarfaty@gmail.com>
2018-06-08 11:06:26 +0100

    Removed scoring system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,31 +5,5 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
-	public Text scoreText;
-
-	private Target[] targets;
-	private int scoreCount;
-
-	void Awake(){
-		targets = GameObject.FindObjectsOfType<Target> ();
-		scoreCount = 0;
-	}
-
-	void Start(){
-		scoreText.text = scoreCount + " / " + targets.Length;
-	}
-
-	public void IncreaseScore(){
-		scoreCount++;
-		scoreText.text = scoreCount + " / " + targets.Length;
-	}
-
-	void Update(){
-		if (scoreCount >= targets.Length) {
-			scoreText.color = Color.yellow;
-		}
-	}
-
-
 
 }
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -6,17 +6,14 @@ using UnityEngine;
 public class Target : MonoBehaviour {
 
 	private Renderer myRenderer;
-	private GameManager gm;
 
 	void Awake(){
 		myRenderer = GetComponent<Renderer> ();
-		gm = GameObject.FindObjectOfType<GameManager> ();
 	}
 
 	public void Acquire(){
 		GetComponent<CapsuleCollider> ().isTrigger = true;
 		myRenderer.material.color = Color.blue;
-		gm.IncreaseScore ();
 	}
 		
 }
