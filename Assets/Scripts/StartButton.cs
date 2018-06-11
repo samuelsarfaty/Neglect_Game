11f691aff95a263aa30708bedb55f354501824b3
Samuel Sarfaty <samuel.sarfaty@gmail.com>
2018-06-11 10:26:00 +0100

    Added ability to stop water

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -1,21 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartButton : MonoBehaviour {
 
 	private Water water;
+	private bool isWaterActive;
+	private Text buttonText;
+
 
 	void Awake(){
 		water = GameObject.FindObjectOfType<Water> ();
+		isWaterActive = false;
+		buttonText = GetComponentInChildren<Text> ();
 	}
 
 	// Use this for initialization
 	void Start () {
-		water.gameObject.SetActive (false);
+		//water.gameObject.SetActive (false);
+		StopWater();
+	}
+
+	public void Activate(){
+		if (!isWaterActive) {
+			StartWater ();
+		} else {
+			StopWater ();
+		}
 	}
 	
-	public void StartWater(){
+	void StartWater(){
 		water.gameObject.SetActive (true);
+		isWaterActive = true;
+		buttonText.text = "Stop";
 	}
+
+	void StopWater(){
+		water.gameObject.SetActive (false);
+		isWaterActive = false;
+		buttonText.text = "Begin!";
+	}
+
+
 }
