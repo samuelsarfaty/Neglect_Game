f14984222eee1d155ef34b4ccc98ae51e1693243
Samuel Sarfaty <samuel.sarfaty@gmail.com>
2018-06-18 15:56:25 +0100

    stuff

--- Assets/Scripts/Pipe.cs
+++ Assets/Scripts/Pipe.cs
@@ -21,7 +21,6 @@ public class Pipe : MonoBehaviour {
 		if (Input.GetMouseButtonDown (0)) {
 			Vector3 rotTarget = new Vector3 (transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y + 90f, transform.localRotation.eulerAngles.z);
 			StartCoroutine(RotateMe(Quaternion.Euler(rotTarget), rotationDuration));
-			//StartCoroutine(RotateMe(Quaternion.Euler(new Vector3(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y + 90f, transform.localRotation.eulerAngles.z)), rotationDuration));
 		
 		} else if (Input.GetMouseButtonDown (1)) {
 			Destroy (transform.parent.gameObject);
@@ -44,25 +43,10 @@ public class Pipe : MonoBehaviour {
 				transform.localPosition = Vector3.Lerp (currentPos, destination, progress);
 				yield return null;
 			}
-		}
 
-		isMoving = false;
+			isMoving = false;
+		}
 	}
-
-	/*IEnumerator RotateMe(Vector3 byAngles, float duration){
-		if (!isMoving) {
-			isMoving = true;
-			var fromAngle = transform.rotation;
-			var toAngle = Quaternion.Euler (transform.eulerAngles + byAngles);
-
-			for (float t = 0; t <= 1; t += Time.deltaTime / duration) {
-				transform.rotation = Quaternion.Slerp (fromAngle, toAngle, t);
-				yield return null;
-			}
-		}
-
-		isMoving = false;
-	}*/
 
 	IEnumerator RotateMe(Quaternion destination, float duration){
 		if (!isMoving) {
@@ -76,6 +60,7 @@ public class Pipe : MonoBehaviour {
 				transform.localRotation = Quaternion.Slerp (startAngle, destination, progress);
 				yield return null;
 			}
+
 			isMoving = false;
 		}
 			
