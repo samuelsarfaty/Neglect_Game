070acab6db6ee0c75809ca6f38cdab3f67bab803
Samuel Sarfaty <samuel.sarfaty@gmail.com>
2018-06-18 14:19:06 +0100

    Pipe setting animation

--- Assets/Scripts/Pipe.cs
+++ Assets/Scripts/Pipe.cs
@@ -4,11 +4,31 @@ using UnityEngine;
 
 public class Pipe : MonoBehaviour {
 
+	[Range(0,3)]
+	public float animDuration;
+
 	void OnMouseOver(){
 		if (Input.GetMouseButtonDown (0)) {
 			transform.Rotate (new Vector3 (0, 90, 0));
 		} else if (Input.GetMouseButtonDown (1)) {
-			Destroy (gameObject);
+			Destroy (transform.parent.gameObject);
 		}
+	}
+
+	void Start(){
+		StartCoroutine (SpawnAnim(Vector3.zero, animDuration));
+	}
+
+	IEnumerator SpawnAnim(Vector3 destination, float duration){
+		Vector3 currentPos = transform.localPosition;
+		float progress = 0f;
+		float startTime = Time.time;
+
+		while (progress < 1f) {
+			progress = (Time.time - startTime) / duration;
+			transform.localPosition = Vector3.Lerp (currentPos, destination, progress);
+			yield return null;
+		}
+
 	}
 }
