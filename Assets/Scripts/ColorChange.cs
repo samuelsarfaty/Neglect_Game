dbaea6f831d099e4e32d7067948f42f6204ac6fa
Samuel Sarfaty <samuel.sarfaty@gmail.com>
2018-06-20 12:18:56 +0100

    Changed positions of UI elements

--- Assets/Scripts/ColorChange.cs
+++ Assets/Scripts/ColorChange.cs
@@ -23,15 +23,16 @@ public class ColorChange : MonoBehaviour {
 
 	void OnGUI()
 	{
-		GUI.Label(new Rect(25, 40, 100, 30), "Red");
-		GUI.Label(new Rect(25, 70, 100, 30), "Green");
-		GUI.Label(new Rect(25, 100, 100, 30), "Blue");
-		GUI.Label(new Rect(25, 130, 100, 30), "Alpha");
 
-		hSliderValueR = GUI.HorizontalSlider(new Rect(95, 45, 100, 30), hSliderValueR, 0.0F, 1.0F);
-		hSliderValueG = GUI.HorizontalSlider(new Rect(95, 75, 100, 30), hSliderValueG, 0.0F, 1.0F);
-		hSliderValueB = GUI.HorizontalSlider(new Rect(95, 105, 100, 30), hSliderValueB, 0.0F, 1.0F);
-		hSliderValueA = GUI.HorizontalSlider(new Rect(95, 135, 100, 30), hSliderValueA, 0.0F, 1.0F);
+		GUI.Label(new Rect(1050, 40, 100, 30), "Red");
+		GUI.Label(new Rect(1050, 70, 100, 30), "Green");
+		GUI.Label(new Rect(1050, 100, 100, 30), "Blue");
+		GUI.Label(new Rect(1050, 130, 100, 30), "Alpha");
+
+		hSliderValueR = GUI.HorizontalSlider(new Rect(1100, 45, 100, 30), hSliderValueR, 0.0F, 1.0F);
+		hSliderValueG = GUI.HorizontalSlider(new Rect(1100, 75, 100, 30), hSliderValueG, 0.0F, 1.0F);
+		hSliderValueB = GUI.HorizontalSlider(new Rect(1100, 105, 100, 30), hSliderValueB, 0.0F, 1.0F);
+		hSliderValueA = GUI.HorizontalSlider(new Rect(1100, 135, 100, 30), hSliderValueA, 0.0F, 1.0F);
 	}
 
 
