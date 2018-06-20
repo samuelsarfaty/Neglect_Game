87e4858f38e3c1374dd0ee736bef52632f95e665
Samuel Sarfaty <samuel.sarfaty@gmail.com>
2018-06-20 14:58:37 +0100

    Refactor water activation to be done by game manager instead of play button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,5 +5,43 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
+	public Mill[] mills;
 
+	private Water water;
+	private StartButton startButton;
+	private bool isWaterActive;
+
+	void Awake(){
+		water = GameObject.FindObjectOfType<Water> ();
+		startButton = GameObject.FindObjectOfType<StartButton> ();
+		isWaterActive = false;
+	}
+
+	void Start(){
+		StopAction ();
+	}
+
+	public void Activate(){
+		if (!isWaterActive) {
+			StartAction ();
+			startButton.SwitchText ();
+		} else {
+			StopAction ();
+			startButton.SwitchText ();
+			foreach (Mill mill in mills) {
+				mill.ResetMill ();
+			}
+		}
+	}
+
+	public void StartAction(){
+		water.gameObject.SetActive (true);
+		isWaterActive = true;
+	}
+
+	public void StopAction(){
+		water.gameObject.SetActive (false);
+		isWaterActive = false;
+		water.hasSplashed = false;
+	}
 }
--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -4,52 +4,32 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class StartButton : MonoBehaviour {
-
-	public Mill myMill;
+	
 	public string startText;
 	public string stopText;
 
-	private Water water;
-	private bool isWaterActive;
 	private Text buttonText;
+	private bool isTextGo;
 
 
 	void Awake(){
-		water = GameObject.FindObjectOfType<Water> ();
-		isWaterActive = false;
 		buttonText = GetComponentInChildren<Text> ();
+		isTextGo = true;
 	}
 
 	// Use this for initialization
 	void Start () {
 		buttonText.text = startText;
-		StopWater();
 	}
 
-	public void Activate(){
-		if (!isWaterActive) {
-			StartWater ();
+	public void SwitchText(){
+		if (isTextGo) {
+			buttonText.text = stopText;
+			isTextGo = false;
 		} else {
-			StopWater ();
-			if (myMill) {
-				myMill.ResetMill ();
-			}
+			buttonText.text = startText;
+			isTextGo = true;
 		}
 	}
-	
-	void StartWater(){
-		water.gameObject.SetActive (true);
-		isWaterActive = true;
-		buttonText.text = stopText;
-
-	}
-
-	void StopWater(){
-		water.gameObject.SetActive (false);
-		isWaterActive = false;
-		buttonText.text = startText;
-		water.hasSplashed = false;
-	}
-
-
+		
 }
